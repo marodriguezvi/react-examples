c7fa309f4d305d38c110bc09438997c148847603
Mauricio Rodriguez <mrodriguezv92@gmail.com>
2022-08-20 19:35:44 -0500

    feat: Rename folder

---
+++ project_1/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.data.date} />
+        <div className="expense-item__description">
+          <h2>{props.data.title}</h2>
+          <div className="expense-item__price">${props.data.amount}</div>
+        </div>
+      </Card>
+    </li>
+  );
+}
+
+export default ExpenseItem;
